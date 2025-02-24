271dfd359011b6609824db6177225c5dac0b3d59
Penguin5681 <pranavsinha922@gmail.com>
2025-02-24 17:42:01 +0530

    feat: add an option for 'Forget Password?' and a 'Sign up option'.

--- lib/views/authentication_screens/login_screen.dart
+++ lib/views/authentication_screens/login_screen.dart
@@ -60,22 +60,35 @@ class LoginScreen extends StatelessWidget {
                     obscureText: true,
                     prefixIcon: Icon(Icons.lock),
                   ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: const Text('Forgot password?'),
+                      ),
+                    ],
+                  ),
                   SizedBox(height: 25),
-                  ProgressiveButtonFlutter(
-                    height: 40,
-                    progressColor: Colors.green,
-                    textStyle: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20.0,
+                  MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: ProgressiveButtonFlutter(
+                      height: 40,
+                      progressColor: Colors.green,
+                      textStyle: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20.0,
+                      ),
+                      backgroundColor: backgroundColor,
+                      text: 'Login',
+                      onPressed: () async {
+                        await Future.delayed(const Duration(seconds: 5));
+                      },
+                      estimatedTime: const Duration(seconds: 5),
+                      elevation: 0,
                     ),
-                    backgroundColor: backgroundColor,
-                    text: 'Login',
-                    onPressed: () async {
-                      await Future.delayed(const Duration(seconds: 5));
-                    },
-                    estimatedTime: const Duration(seconds: 5),
-                    elevation: 0,
                   ),
                   SizedBox(height: 20),
                   Row(
@@ -101,6 +114,20 @@ class LoginScreen extends StatelessWidget {
                   ),
                   SizedBox(height: 20),
                   GoogleButton(),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don\'t have an account?  '),
+                      MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
