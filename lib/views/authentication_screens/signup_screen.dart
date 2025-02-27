9031f88984603ccb425c7762b495b162a71c44a3
Penguin5681 <pranavsinha922@gmail.com>
2025-02-27 09:48:34 +0530

    feat: the app will now allow users to signup using firebase.

--- lib/views/authentication_screens/signup_screen.dart
+++ lib/views/authentication_screens/signup_screen.dart
@@ -1,42 +1,80 @@
+import 'package:deep_sage/providers/api/auth_api_provider.dart';
 import 'package:deep_sage/views/authentication_screens/login_screen.dart';
 import 'package:deep_sage/widgets/dev_fab.dart';
 import 'package:deep_sage/widgets/google_button.dart';
 import 'package:deep_sage/widgets/primary_edit_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:progressive_button_flutter/progressive_button_flutter.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
-  // Route _createRoute() {
-  //   return PageRouteBuilder(
-  //     pageBuilder: (context, animation, secondaryAnimation) => LoginScreen(),
-  //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-  //       const begin = Offset(-1.0, 0.0);
-  //       const end = Offset.zero;
-  //       const curve = Curves.ease;
-  //
-  //       var tween = Tween(
-  //         begin: begin,
-  //         end: end,
-  //       ).chain(CurveTween(curve: curve));
-  //       return SlideTransition(position: animation.drive(tween), child: child);
-  //     },
-  //   );
-  // }
+  Route createScreenRoute(Widget screen, double deltaX, double deltaY) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => screen,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(deltaX, deltaY);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        return SlideTransition(position: animation.drive(tween), child: child);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final env = dotenv.env['FLUTTER_ENV'];
+
     final TextEditingController emailController = TextEditingController();
     final TextEditingController passwordController = TextEditingController();
-    final TextEditingController confirmPasswordController =
-        TextEditingController();
+    final TextEditingController confirmPasswordController = TextEditingController();
 
     final backgroundColor =
-        Theme.of(
-          context,
-        ).elevatedButtonTheme.style?.backgroundColor?.resolve({}) ??
-        Colors.black;
+        Theme.of(context).elevatedButtonTheme.style?.backgroundColor?.resolve({}) ?? Colors.black;
+
+    Future<void> validateCredentials(String email, String password, String confirmPassword) async {
+      final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+      if (emailRegex.hasMatch(email)) {
+        if (password.length > 6 && confirmPassword.length > 6) {
+          if (password == confirmPassword) {
+            await AuthApiProvider()
+                .firebaseUserSignUp(emailController.text, passwordController.text)
+                .then((result) {
+                  if (!context.mounted) return;
+                  showTopSnackBar(
+                    Overlay.of(context),
+                    CustomSnackBar.success(message: result['message']),
+                  );
+                  Navigator.of(context).pushReplacement(createScreenRoute(LoginScreen(), 1.0, 0));
+                });
+          } else {
+            if (!context.mounted) return;
+            showTopSnackBar(
+              Overlay.of(context),
+              CustomSnackBar.success(message: 'Passwords do not match'),
+            );
+          }
+        } else {
+          if (!context.mounted) return;
+          showTopSnackBar(
+            Overlay.of(context),
+            CustomSnackBar.success(message: 'Password too short'),
+          );
+        }
+      } else {
+        if (!context.mounted) return;
+        showTopSnackBar(
+          Overlay.of(context),
+          CustomSnackBar.success(message: 'Please enter a valid email'),
+        );
+      }
+    }
+
     return Scaffold(
       floatingActionButton: DevFAB(parentContext: context),
       body: Center(
@@ -45,11 +83,7 @@ class SignupScreen extends StatelessWidget {
           children: [
             Text(
               'Deep Sage',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 35.0,
-                letterSpacing: 4.0,
-              ),
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 35.0, letterSpacing: 4.0),
             ),
             Text(
               'Empowering Data Science with AI',
@@ -95,7 +129,17 @@ class SignupScreen extends StatelessWidget {
                       backgroundColor: backgroundColor,
                       text: 'Sign Up',
                       onPressed: () async {
-                        await Future.delayed(const Duration(seconds: 5));
+                        if (env == 'production') {
+                          await validateCredentials(
+                            emailController.text,
+                            passwordController.text,
+                            confirmPasswordController.text,
+                          );
+                        } else {
+                          Navigator.of(
+                            context,
+                          ).pushReplacement(createScreenRoute(LoginScreen(), -1.0, 0));
+                        }
                       },
                       estimatedTime: const Duration(seconds: 5),
                       elevation: 0,
@@ -106,21 +150,14 @@ class SignupScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Divider(color: Colors.green, thickness: 1),
-                      ),
+                      Expanded(child: Divider(color: Colors.green, thickness: 1)),
                       const Padding(
                         padding: EdgeInsets.symmetric(horizontal: 10.0),
                         child: Center(
-                          child: Text(
-                            'or continue with',
-                            style: TextStyle(fontSize: 16),
-                          ),
+                          child: Text('or continue with', style: TextStyle(fontSize: 16)),
                         ),
                       ),
-                      Expanded(
-                        child: Divider(color: Colors.green, thickness: 1),
-                      ),
+                      Expanded(child: Divider(color: Colors.green, thickness: 1)),
                     ],
                   ),
                   SizedBox(height: 20),
@@ -134,18 +171,11 @@ class SignupScreen extends StatelessWidget {
                         cursor: SystemMouseCursors.click,
                         child: GestureDetector(
                           onTap: () {
-                            Navigator.push(
+                            Navigator.of(
                               context,
-                              MaterialPageRoute(
-                                builder:
-                                    (BuildContext context) => LoginScreen(),
-                              ),
-                            );
+                            ).pushReplacement(createScreenRoute(LoginScreen(), -1.0, 0));
                           },
-                          child: Text(
-                            'Sign In',
-                            style: TextStyle(color: Colors.blue),
-                          ),
+                          child: Text('Sign In', style: TextStyle(color: Colors.blue)),
                         ),
                       ),
                     ],
