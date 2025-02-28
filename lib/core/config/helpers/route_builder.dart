c24852178326357557999e07278ba3f1e25b09f5
Penguin5681 <pranavsinha922@gmail.com>
2025-02-28 08:47:52 +0530

    feat: made the search public dataset btn working and added an extra prop to dashboard_screen.dart

---
+++ lib/core/config/helpers/route_builder.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RouteBuilder {
+  Route build(Widget screen, double deltaX, double deltaY) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => screen,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(deltaX, deltaY);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween = Tween(
+          begin: begin,
+          end: end,
+        ).chain(CurveTween(curve: curve));
+        return SlideTransition(position: animation.drive(tween), child: child);
+      },
+    );
+  }
+}
