8d5551052c59090ef39e8411bf7a96eb87394087
Penguin5681 <pranavsinha922@gmail.com>
2025-03-04 12:43:18 +0530

    feat: added the file picker on the dashboard

--- lib/views/core_screens/dashboard_screen.dart
+++ lib/views/core_screens/dashboard_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:deep_sage/core/config/helpers/app_icons.dart';
 import 'package:deep_sage/views/core_screens/search_screens/search_screen.dart';
 import 'package:deep_sage/views/core_screens/settings_screen.dart';
 import 'package:deep_sage/widgets/dataset_card.dart';
 import 'package:deep_sage/widgets/dev_fab.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -32,19 +35,11 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   final navigatorKey = GlobalKey<NavigatorState>();
 
-  Widget getIconForTheme({
-    required String lightIcon,
-    required String darkIcon,
-    double size = 24,
-  }) {
+  Widget getIconForTheme({required String lightIcon, required String darkIcon, double size = 24}) {
     return Builder(
       builder: (context) {
         final isDarkMode = Theme.of(context).brightness == Brightness.dark;
-        return Image.asset(
-          isDarkMode ? darkIcon : lightIcon,
-          width: size,
-          height: size,
-        );
+        return Image.asset(isDarkMode ? darkIcon : lightIcon, width: size, height: size);
       },
     );
   }
@@ -93,13 +88,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       SizedBox(height: 40),
                       MouseRegion(
                         cursor: SystemMouseCursors.click,
-                        child: ClipOval(
-                          child: Image.asset(
-                            AppIcons.larry,
-                            width: 40,
-                            height: 40,
-                          ),
-                        ),
+                        child: ClipOval(child: Image.asset(AppIcons.larry, width: 40, height: 40)),
                       ),
                     ],
                   ),
@@ -203,16 +192,25 @@ class _DashboardScreenState extends State<DashboardScreen> {
           Expanded(child: screens[selectedIndex]),
         ],
       ),
-      floatingActionButton:
-          env == 'development' ? DevFAB(parentContext: context) : null,
+      floatingActionButton: env == 'development' ? DevFAB(parentContext: context) : null,
     );
   }
 }
 
-class Dashboard extends StatelessWidget {
+// the actual screen starts from here for the dashboard
+
+class Dashboard extends StatefulWidget {
   final Function(int) onNavigate;
 
   const Dashboard({super.key, required this.onNavigate});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  late var isDatasetUploaded = false;
+  late var datasetPath = '';
 
   @override
   Widget build(BuildContext context) {
@@ -228,69 +226,127 @@ class Dashboard extends StatelessWidget {
                 children: [
                   const Text(
                     'Welcome back, Larry',
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(height: 25),
                   Row(
                     children: [
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          FilePickerResult? result = await FilePicker.platform.pickFiles(
+                            dialogTitle: "Import a dataset",
+                            lockParentWindow: true,
+                            type: FileType.custom,
+                            allowedExtensions: ["json", "xlsx", "csv"],
+                          );
+                          if (result != null) {
+                            File file = File(result.files.single.path!);
+                            setState(() {
+                              isDatasetUploaded = true;
+                              datasetPath = file.path;
+                            });
+                            debugPrint(file.path);
+                          } else {
+                            debugPrint('file operation cancelled');
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.blue.shade600,
                           foregroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 12,
-                          ),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                         ),
                         child: const Text(
                           "Upload Dataset",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                       ),
                       const SizedBox(width: 10),
                       OutlinedButton(
                         onPressed: () {
-                          onNavigate(1);
+                          widget.onNavigate(1);
                         },
                         style: OutlinedButton.styleFrom(
-                          side: BorderSide(
-                            color: Colors.blue.shade600,
-                            width: 2,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                          side: BorderSide(color: Colors.blue.shade600, width: 2),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                           foregroundColor: Colors.blue.shade600,
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 12,
-                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                         ),
                         child: const Text(
                           "Search Public Datasets",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ],
                   ),
                   const SizedBox(height: 20),
-                  const Text(
-                    'Recent Datasets',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
+                  // current uploaded dataset
+                  if (isDatasetUploaded)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Dataset successfully uploaded',
+                              style: TextStyle(
+                                color: Colors.blue.shade600,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22.0,
+                              ),
+                            ),
+                            const SizedBox(width: 25),
+                            MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              child: GestureDetector(
+                                onTap: () async {
+                                  // reset states
+                                  setState(() {
+                                    isDatasetUploaded = false;
+                                  });
+                                  FilePickerResult? result = await FilePicker.platform.pickFiles(
+                                    dialogTitle: "Import a dataset",
+                                    lockParentWindow: true,
+                                    type: FileType.custom,
+                                    allowedExtensions: ["json", "xlsx", "csv"],
+                                  );
+                                  if (result != null) {
+                                    File file = File(result.files.single.path!);
+                                    setState(() {
+                                      isDatasetUploaded = true;
+                                      datasetPath = file.path;
+                                    });
+                                    debugPrint(file.path);
+                                  } else {
+                                    debugPrint('file operation cancelled');
+                                  }
+                                },
+                                child: Text(
+                                  'Change dataset',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.0,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 16.0),
+                        DatasetCard(
+                          lightIconPath: AppIcons.checkLight,
+                          labelText: 'Current Dataset',
+                          subLabelText: datasetPath,
+                          buttonText: 'Analyze',
+                          darkIconPath: AppIcons.checkDark,
+                        ),
+                      ],
+                    ),
+
+                  const SizedBox(height: 16.0),
+                  const Text('Recent Datasets', style: TextStyle(fontSize: 20.0)),
                   const SizedBox(height: 20),
                   Listener(
                     onPointerSignal: (PointerSignalEvent event) {
