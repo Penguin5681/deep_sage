dd4687c0dad7e70855d8b8b4adbc25941be2057b
Samarth Garge <gargesamarth@gmail.com>
2025-03-10 13:32:26 +0530

    Implemented the feature of issue #91
    But I have added one line in the popular_datasets.dart file, due to which the popular_datasets of kaggle are not been able to display.
    Everything else is working.

--- lib/views/core_screens/search_screens/search_category_screens/category_health.dart
+++ lib/views/core_screens/search_screens/search_category_screens/category_health.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class CategoryHealthcare extends StatelessWidget {
+  final Function(String) onSearch;
+
+  const CategoryHealthcare({super.key, required this.onSearch});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Healthcare Category'),
+    );
+  }
+}
